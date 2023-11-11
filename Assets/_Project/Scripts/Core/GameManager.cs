eb3b30e2efe0a89c4679dac8752484ccf6b6f3dd
Bohdan Tsaplia <bohdan.tsaplia@udtech.global>
2023-11-11 21:28:33 +0200

    add hive & win handling

---
+++ Assets/_Project/Scripts/Core/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using gameoff.World;
+using UnityEngine;
+
+namespace gameoff.Core
+{
+    public class GameManager : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            Hive.Died += OnHiveDied;
+        }
+
+        private void OnDisable()
+        {
+            Hive.Died -= OnHiveDied;
+        }
+        
+        private void OnHiveDied(Hive hive)
+        {
+            var hives = FindObjectsOfType<Hive>().Where(x => x != hive).ToArray();
+            if (hives.Length <= 0) 
+                Win();
+        }
+
+        private void Lose()
+        {
+            Debug.Log("Lose");
+        }
+
+        private void Win()
+        {
+            Debug.Log("Win");
+        }
+    }
+}
