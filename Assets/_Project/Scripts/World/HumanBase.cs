2d16e3e38bd68e4cba00b950628a04a470907bf3
Bohdan Tsaplia <bohdan.tsaplia@udtech.global>
2023-11-18 15:18:01 +0200

    change creep map  & human base clear creep on start

--- Assets/_Project/Scripts/World/HumanBase.cs
+++ Assets/_Project/Scripts/World/HumanBase.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using gameoff.Enemy.Projectiles;
 using UnityEngine;
+using Zenject;
 
 namespace gameoff.World
 {
     public class HumanBase : MonoBehaviour
     {
+        [SerializeField] private int spawnClearCreepRadius = 300;
+        
+        [Inject] private ICreepClearing _creepClearing;
+        
         public static event Action CreepReachedBase;
         private bool _isCreepReached;
-        
+
+        private void Start() => _creepClearing.ClearCreep(transform.position, spawnClearCreepRadius);
+
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (_isCreepReached)
