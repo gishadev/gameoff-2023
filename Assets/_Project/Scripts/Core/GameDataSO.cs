5620ebbfbfcf7dce66d7803431f226c2a81b1760
Bohdan Tsaplia <bohdan.tsaplia@udtech.global>
2023-11-25 00:13:07 +0200

    upgrades giving rules inspector

--- Assets/_Project/Scripts/Core/GameDataSO.cs
+++ Assets/_Project/Scripts/Core/GameDataSO.cs
@@ -1,6 +1,7 @@
 ﻿using gameoff.PlayerManager;
 using gishadev.tools.Events;
 using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
 using Sirenix.Serialization;
 using UnityEngine;
 
@@ -9,14 +10,51 @@ namespace gameoff.Core
     [CreateAssetMenu(fileName = "GameDataSO", menuName = "ScriptableObjects/GameDataSO")]
     public class GameDataSO : SerializedScriptableObject
     {
-        [TabGroup("Abilities"), InlineEditor]
-        [OdinSerialize, ShowInInspector] public DashAbilityDataSO DashAbilityDataSO { private set; get; }
-        [TabGroup("Abilities"), InlineEditor]
-        [OdinSerialize, ShowInInspector] public ExplosionAbilityDataSO ExplosionAbilityDataSO { private set; get; }
+        [Title("Upgrades giving rules")]
+        [TabGroup("Upgrades"), InfoBox("Winning every [x] level will give to player an upgrade to choose")]
+        [OdinSerialize, ShowInInspector, PropertyOrder(-1)]
+        public int UpgradesStep { private set; get; }
+
+        [TabGroup("Upgrades")]
+        [SerializeField, ShowInInspector, OnCollectionChanged(after: nameof(OnPackChanged))]
+        private UpgradesPack[] upgradesPack;
+
+        public UpgradesPack[] UpgradesPack => upgradesPack;
+
+        [Title("Upgrades Settings")]
+        [TabGroup("Upgrades"), InlineEditor]
+        [OdinSerialize, ShowInInspector]
+        public DashAbilityDataSO DashAbilityDataSO { private set; get; }
+
+        [TabGroup("Upgrades"), InlineEditor]
+        [OdinSerialize, ShowInInspector]
+        public ExplosionAbilityDataSO ExplosionAbilityDataSO { private set; get; }
+
 
         [TabGroup("UI")]
-        [OdinSerialize, ShowInInspector, AssetsOnly] public GameObject UpgradeCardPrefab  { private set; get; }
-        
-        [field: SerializeField] public DefaultEventChannelSO UnlockEventChannel { private set; get; }
+        [OdinSerialize, ShowInInspector, AssetsOnly]
+        public GameObject UpgradeCardPrefab { private set; get; }
+
+        [TabGroup("UI")]
+        [field: SerializeField]
+        public DefaultEventChannelSO UnlockEventChannel { private set; get; }
+
+        public void OnPackChanged(CollectionChangeInfo info, object value)
+        {
+            if (info.ChangeType != CollectionChangeType.Add)
+                return;
+
+            Debug.Log("Received callback AFTER CHANGE with the following info: " + info +
+                      ", and the following collection instance: " + value);
+
+            var newPack = info.Value as UpgradesPack;
+            if (newPack == null)
+                return;
+            
+            if (upgradesPack.Length > 1)
+                newPack.TargetLevelNumber = upgradesPack[^2].TargetLevelNumber + UpgradesStep;
+            else
+                newPack.TargetLevelNumber = UpgradesStep;
+        }
     }
 }
