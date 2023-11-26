2a49c915b8eeb670f4e4b08e617a3b08c04308d5
Bohdan Tsaplia <bohdan.tsaplia@udtech.global>
2023-11-26 02:24:36 +0200

    projectile shield WIP

---
+++ Assets/_Project/Scripts/Enemy/EnemyProjectileShield.cs
@@ -0,0 +1,26 @@
+﻿using gameoff.PlayerManager;
+using UnityEngine;
+
+namespace gameoff.Enemy
+{
+    public class EnemyProjectileShield : MonoBehaviour
+    {
+        [SerializeField] private Collider2D projectileShieldCollider;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (!other.TryGetComponent(out Player player))
+                return;
+
+            projectileShieldCollider.enabled = false;
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (!other.TryGetComponent(out Player player))
+                return;
+            
+            projectileShieldCollider.enabled = true;
+        }
+    }
+}
