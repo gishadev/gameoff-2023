1ba4de3054a93f00296138390f61803dc54d8476
Bohdan Tsaplia <bohdan.tsaplia@udtech.global>
2023-11-26 21:42:17 +0200

    smart tiles & convert first level

---
+++ Assets/_Project/Scripts/World/Level.cs
@@ -0,0 +1,16 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace gameoff.World
+{
+    public class Level : MonoBehaviour
+    {
+        [ValidateInput(nameof(BaseMustBeNotNull), "This field must not be null.")]
+        [SerializeField] private HumanBase humanBase;
+        [ValidateInput(nameof(CreepMustBeNotNull), "This field must not be null.")]
+        [SerializeField] private Creep creep;
+        
+        private bool BaseMustBeNotNull(HumanBase obj) => obj != null;
+        private bool CreepMustBeNotNull(Creep obj) => obj != null;
+    }
+}
---
+++ Assets/_Project/Scripts/World/LevelConverter.cs
@@ -0,0 +1,25 @@
+﻿using Sirenix.OdinInspector;
+using UnityEditor;
+using UnityEngine;
+
+namespace gameoff.World
+{
+    public class LevelConverter : MonoBehaviour
+    {
+        [SerializeField] private GameObject spawnSpotPrefab;
+        [SerializeField] private Transform spawnSpotsParent;
+        
+        [Button("Enemies into spawn spots")]
+        public void ConvertEnemiesIntoSpawnSpots()
+        {
+            var enemies = FindObjectsOfType<Enemy.Enemy>();
+
+            foreach (var enemy in enemies)
+            {
+                var prefabObject = PrefabUtility.InstantiatePrefab(this.spawnSpotPrefab, spawnSpotsParent) as GameObject;
+                prefabObject.transform.position = enemy.transform.position;
+                enemy.gameObject.SetActive(false);
+            }
+        }
+    }
+}
