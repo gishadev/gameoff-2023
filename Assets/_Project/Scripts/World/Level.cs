6c0e8335ae1ea416dd76d0a44589c13b23d3328f
Bohdan Tsaplia <bohdan.tsaplia@udtech.global>
2023-11-27 21:50:33 +0200

    level bounds & camera confiner

--- Assets/_Project/Scripts/World/Level.cs
+++ Assets/_Project/Scripts/World/Level.cs
@@ -1,4 +1,5 @@
-﻿using Sirenix.OdinInspector;
+﻿using System;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace gameoff.World
@@ -9,11 +10,16 @@ namespace gameoff.World
         [SerializeField] private HumanBase humanBase;
         [ValidateInput(nameof(CreepMustBeNotNull), "This field must not be null.")]
         [SerializeField] private Creep creep;
+        [ValidateInput(nameof(LevelBoundsMustBeNotNull), "This field must not be null.")]
+        [SerializeField] private LevelBounds levelBounds;
 
         public HumanBase HumanBase => humanBase;
         public Creep Creep => creep;
-
+        public LevelBounds LevelBounds => levelBounds;
+        
+        
         private bool BaseMustBeNotNull(HumanBase obj) => obj != null;
         private bool CreepMustBeNotNull(Creep obj) => obj != null;
+        private bool LevelBoundsMustBeNotNull(LevelBounds obj) => obj != null;
     }
 }
