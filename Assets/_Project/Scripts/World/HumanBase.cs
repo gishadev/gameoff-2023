891653244352a2e20ac4eade0a6a0c4cbee9e360
Bohdan Tsaplia <bohdan.tsaplia@udtech.global>
2023-11-29 22:04:53 +0200

    fixes & new base

--- Assets/_Project/Scripts/World/HumanBase.cs
+++ Assets/_Project/Scripts/World/HumanBase.cs
@@ -8,13 +8,17 @@ namespace gameoff.World
     public class HumanBase : MonoBehaviour
     {
         [SerializeField] private int spawnClearCreepRadius = 300;
+        [field: SerializeField] public Transform Spawnpoint { private set; get; }
         
         [Inject] private ICreepClearing _creepClearing;
         
         public static event Action CreepReachedBase;
         private bool _isCreepReached;
 
-        private void Start() => _creepClearing.ClearCreep(transform.position, spawnClearCreepRadius);
+        private void Start()
+        {
+            _creepClearing.ClearCreep(transform.position, spawnClearCreepRadius);
+        }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
